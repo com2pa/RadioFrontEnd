2d59cda45110223dd81863a63a2db0386987e98a
merwil vegas <com2pa@gmail.com>
2025-10-19 22:11:17 -0400

    llamando a las secciones para el home en desarrollo

--- src/page/public/Home.jsx
+++ src/page/public/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
-import PublicFooter from '../../components/layout/PublicFooter'
+import { Box, useColorModeValue } from '@chakra-ui/react'
 import PublicLayout from '../../components/layout/PublicLayout'
+import PublicFooter from '../../components/layout/PublicFooter'
+import HeroSection from '../../components/sections/HeroSection'
+import LivePrograms from '../../components/sections/LivePrograms'
+import TopSongs from '../../components/sections/TopSongs'
+import StatsSection from '../../components/sections/StatsSection'
+import CategoriesSection from '../../components/sections/CategoriesSection'
 
 const Home = () => {
+  const bgColor = useColorModeValue('gray.50', 'gray.900')
+
   return (
     <PublicLayout>
-      <Outlet />
+      <Box bg={bgColor} minH="100vh">
+        <HeroSection />
+        <LivePrograms />
+        <TopSongs />
+        <StatsSection />
+        <CategoriesSection />
+      </Box>
       <PublicFooter />
     </PublicLayout>
   )
